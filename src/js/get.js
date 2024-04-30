812240ca32304f3f6b8e96d7350f6034831309ff
Jocke <joakim03moller@gmail.com>
2024-04-30 09:38:25 +0200

    Lagt till laddningsanimation vid fetch

--- src/js/get.js
+++ src/js/get.js
@@ -2,29 +2,34 @@
 import { deleteWorkExperience } from "./delete";
 
 /* Kör fetchWorkExperiences när sidan laddats in. */
-document.addEventListener('DOMContentLoaded', () => {
-    fetchWorkExperiences();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchWorkExperiences();
 });
 
 /* Hämtar all data från api/work-experiences */
 export async function fetchWorkExperiences() {
-    try {
-        const response = await fetch ('https://backend-moment3-pbtd.onrender.com/api/work-experiences');
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error(data.message) || 'Failed to fetch work experiences.'
-        }
+  try {
+    showLoadingSpinner();
+    const response = await fetch(
+      "https://backend-moment3-pbtd.onrender.com/api/work-experiences"
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message) || "Failed to fetch work experiences.";
+    }
 
-        /* Hämtar listan och nollställer */
-        const workExperiencesList = document.getElementById('work-experiences-list');
-        workExperiencesList.innerHTML = '';
+    /* Hämtar listan och nollställer */
+    const workExperiencesList = document.getElementById(
+      "work-experiences-list"
+    );
+    workExperiencesList.innerHTML = "";
 
-        data.forEach(workExperience => {
-            const startdate = workExperience.startdate.split("T")[0];
-            const enddate = workExperience.enddate.split("T")[0];
-            /* Information från fetch */
-            const listItem = document.getElementById('work-experiences-list');
-            listItem.innerHTML += `<li><strong>Företagsnamn:</strong> ${workExperience.companyname} <br>
+    data.forEach((workExperience) => {
+      const startdate = workExperience.startdate.split("T")[0];
+      const enddate = workExperience.enddate.split("T")[0];
+      /* Information från fetch */
+      const listItem = document.getElementById("work-experiences-list");
+      listItem.innerHTML += `<li><strong>Företagsnamn:</strong> ${workExperience.companyname} <br>
             <strong>Jobbtitel:</strong> ${workExperience.jobtitle}<br>
             <strong>Plats:</strong> ${workExperience.location}<br>
             <strong>Startdatum:</strong> ${startdate}<br>
@@ -32,20 +37,30 @@ export async function fetchWorkExperiences() {
             <strong>Beskrivning:</strong> ${workExperience.description}<br>
             <button class="delete-button" data-post-id="${workExperience._id}">TA BORT</button
             </li>`;
-        });
-        attachDeleteEventListeners();
+    });
+    attachDeleteEventListeners();
 
-        /* Funktion för att ta bort specifikt jobbid från webbplatsen */
-        function attachDeleteEventListeners() {
-            const deleteButtons = document.querySelectorAll('.delete-button')
-            deleteButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                    const postID = button.getAttribute('data-post-id');
-                    deleteWorkExperience(postID);
-                });
-            });
-        }
-    } catch (error) {
-        console.error('Error fetching work experiences: ' + error.message);
+    /* Funktion för att ta bort specifikt jobbid från webbplatsen */
+    function attachDeleteEventListeners() {
+      const deleteButtons = document.querySelectorAll(".delete-button");
+      deleteButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          const postID = button.getAttribute("data-post-id");
+          deleteWorkExperience(postID);
+        });
+      });
     }
+    hideLoadingSpinner();
+  } catch (error) {
+    console.error("Error fetching work experiences: " + error.message);
+  }
+}
+
+/* Laddningsanimation som visar spinnern */
+function showLoadingSpinner() {
+  document.getElementById("loadingSpinner").style.display = "block";
+}
+/* Laddningsanimation som gömmer spinnern */
+function hideLoadingSpinner() {
+  document.getElementById("loadingSpinner").style.display = "none";
 }
